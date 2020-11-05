6c84b5b2daff4f0b713209d507fb4c9f620eb607
Tratut <64106392+Tratut@users.noreply.github.com>
2020-11-05 10:17:17 +0300

    Add files via upload

--- ft_rev_list.py
+++ ft_rev_list.py
@@ -5,7 +5,7 @@ def len_list(mass):
     return i
 
 
-def ft_rev_par_list(mass):
+def ft_rev_list(mass):
     long = len_list(mass)
     for i in range(len_list(mass) // 2):
         x = mass[0 + i]
@@ -14,4 +14,4 @@ def ft_rev_par_list(mass):
         mass[long - i - 1] = x
     return mass
 
-# print(ft_rev_par_list([1, 2, 3, 4, 5, 6]))
+# print(ft_rev_list([1, 2, 3, 4, 5, 6]))
